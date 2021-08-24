053aee8bea72f87abbf04f691372f370ed8b8c05
matsunawaryuya <ruymtnw@gmail.com>
2021-08-25 00:41:10 +0900

    feat: 数独の盤面表示

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <table class="sudoku">
+      <tr
+        v-for="(line, lineIndex) in numbers"
+        :key="lineIndex"
+        :class="{
+          'border-bottom': lineIndex == 2 || lineIndex == 5,
+        }"
+      >
+        <td
+          v-for="(number, numberIndex) in line"
+          :key="numberIndex"
+          :class="{
+            'border-right': numberIndex == 2 || numberIndex == 5,
+          }"
+        >
+          {{ number == 0 ? "" : number }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      numbers: [
+        [0, 0, 5, 3, 0, 0, 0, 0, 0],
+        [8, 0, 0, 0, 0, 0, 0, 2, 0],
+        [0, 7, 0, 0, 1, 0, 5, 0, 0],
+        [4, 0, 0, 0, 0, 5, 3, 0, 0],
+        [0, 1, 0, 0, 7, 0, 0, 0, 6],
+        [0, 0, 3, 2, 0, 0, 0, 8, 0],
+        [0, 6, 0, 5, 0, 0, 0, 0, 9],
+        [0, 0, 4, 0, 0, 0, 0, 3, 0],
+        [0, 0, 0, 0, 0, 9, 7, 0, 0],
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  box-sizing: border-box;
   margin-top: 60px;
+  .sudoku {
+    margin: 0 auto;
+    border-collapse: collapse;
+    border: 3px solid #ccc;
+    td {
+      border: 1px solid #ccc;
+      height: 80px;
+      width: 80px;
+      text-align: center;
+      vertical-align: middle;
+      font-size: 55px;
+      cursor: pointer;
+    }
+  }
+}
+.border-bottom {
+  border-bottom: 3px solid #ccc;
+}
+.border-right {
+  border-right: 3px solid #ccc !important;
 }
 </style>
